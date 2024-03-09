33cef9d6d6646f69177f7242af5706979fd57ac3
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-08 17:21:57 -0800

    new push

--- src/screen/Main/Home/HomeScreen.jsx
+++ src/screen/Main/Home/HomeScreen.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function HomeScreen() {
     return (
-        <div>HomeScreen</div>
+        <div className='p-3'>
+            <h1 className='font-bold text-2xl text-black'>Favour NotePad</h1>
+        </div>
     )
 }
 
