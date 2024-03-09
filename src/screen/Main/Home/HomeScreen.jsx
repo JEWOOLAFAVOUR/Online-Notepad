a3300859b40bed3e43d551c053efe9ec108d84fa
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-08 17:32:38 -0800

    new push

--- src/screen/Main/Home/HomeScreen.jsx
+++ src/screen/Main/Home/HomeScreen.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { IoAddSharp } from "react-icons/io5";
+
 export default function HomeScreen() {
     return (
         <div className='p-3'>
-            <h1 className='font-bold text-2xl text-black'>Favour NotePad</h1>
+            <div className='flex justify-between'>
+                <h1 className='font-bold text-xl text-black'>Favour NotePad</h1>
+                <div className='flex items-center px-5 py-3 rounded bg-black hover:opacity-80 cursor-pointer'>
+                    <p className='text-white text-xs'>Add</p>
+                    <IoAddSharp color='white' className='ml-2' />
+                </div>
+            </div>
+            {/* DETAILS BELOW */}
+
+
         </div>
     )
 }
